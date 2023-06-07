56be83f291dc9a2b8a77f811d0f2823411d80382
Kyle Stanfield <kylesonfon@gmail.com>
2023-06-06 22:09:49 -0700

    Add worker limit to async scan

--- scan.py
+++ scan.py
@@ -6,31 +6,47 @@ import time
 async def connectPort(host, port):
     coroutine = asyncio.open_connection(host, port)
     try:
-        reader, writer = await asyncio.wait_for(coroutine, 2.0)
+        _, writer = await asyncio.wait_for(coroutine, 2.0)
         writer.close()
         return True
     except asyncio.TimeoutError:
         return False
 
+async def worker(host, queue):
+    while True:
+        port = await queue.get()
+        if port is None:
+            queue.task_done()
+            return
+        if await connectPort(host, port):
+            print(f"Port {port}: {socket.getservbyport(port)} OPEN")
+        queue.task_done()
+
 async def main():
     argv = sys.argv
     argc = len(argv)
-    if argc != 3:
-        print("Usage: python scan.py $Hostname $portStart-portEnd")
+    if argc < 3 or argc > 4:
+        print("Usage: python scan.py $Hostname $portStart-portEnd [$WORKERS]")
         sys.exit(1)
 
     host = argv[1]
     portRange = argv[2]
     portStart, portEnd = list(map(lambda x: int(x), portRange.split('-')))
+    workerLimit = 256
+    if argc == 4:
+        workerLimit = int(argv[3])
 
-    coroutines = [connectPort(host, p) for p in range(portStart, portEnd+1)]
+    queue = asyncio.Queue()
     startTime = time.time()
-    results = await asyncio.gather(*coroutines)
-    endTime = time.time()
 
-    for port, result in zip(range(portStart, portEnd), results):
-        if result:
-            print(f"Port {port}: {socket.getservbyport(port)} OPEN")
+    workers = [asyncio.create_task(worker(host, queue)) for _ in range(workerLimit)]
+    for port in range(portStart, portEnd+1):
+        await queue.put(port)
+    for _ in range(workerLimit):
+        await queue.put(None)
+    await queue.join()
+    
+    endTime = time.time()
     print(f"Scanned {portEnd-portStart+1} ports in {'{:.2f}'.format(endTime-startTime)} seconds.")
 
 
